36aff4442f3478b8e6c452c8a1823b8ec185213a
pawanrai9999 <pawanrai9999@gmail.com>
2017-01-20 22:26:51 +0530

    This uses datetime module to take current date and time and print date and time with "Hello World"Message

---
+++ printHelloWorld.py
@@ -0,0 +1,11 @@
+# Prints Hello World with date and time
+
+# Module required to get current date and time
+import datetime
+
+
+print(datetime.datetime.now())
+#Gets current date and time to print
+
+print('\n')
+print('Hello World')
