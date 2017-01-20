ccf3df4d52b298a0e7f5b090b683e994be54586c
pawanrai9999 <pawanrai9999@gmail.com>
2017-01-20 22:42:40 +0530

    Added a print statement to print current directory

--- printHelloWorld.py
+++ printHelloWorld.py
@@ -2,6 +2,11 @@
 
 # Module required to get current date and time
 import datetime
+# Module to get location of file
+import os
 
+#Gets current directory path
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print(dir_path)
 print('Hello World')
 print(datetime.datetime.now()) # Presents current date and time.
